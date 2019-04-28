b4df6934031990a7c965b02f63de851c2e25de92
zjm <zoujimao@yofish.com>
2019-04-28 09:37:19 +0800

    增加用户信息DTO，包含了用户的用户类型等信息

---
+++ cardequity-promotion-api/src/main/java/com/youyu/cardequity/promotion/dto/req/UserInfo4CouponIssueDto.java
@@ -0,0 +1,28 @@
+
+package com.youyu.cardequity.promotion.dto.req;
+
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Setter
+@Getter
+public class UserInfo4CouponIssueDto implements Serializable {
+    private static final long serialVersionUID = -8243034458678761124L;
+
+    @ApiModelProperty("用户ID")
+    private String clientId;
+
+    @ApiModelProperty("用户类型:10-普通用户 11-会员用户 12-赠送会员用户")
+    private String userType;
+
+
+    //todo 暂时用不到，保留字段，以后确认用不到，请删除
+    @ApiModelProperty("是否是新用户")
+    private String newUserFlag;
+
+
+}
