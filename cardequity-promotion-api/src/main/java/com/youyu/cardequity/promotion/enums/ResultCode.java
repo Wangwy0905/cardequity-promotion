3f3b3ffd6c5038e9ba88045aabd3a4780a71228c
pqqanqing <15601870402@163.com>
2019-05-08 18:42:31 +0800

    bug fix

--- cardequity-promotion-api/src/main/java/com/youyu/cardequity/promotion/enums/ResultCode.java
+++ cardequity-promotion-api/src/main/java/com/youyu/cardequity/promotion/enums/ResultCode.java
@@ -59,7 +59,7 @@ public enum ResultCode implements CardequityIBaseResultCode {
     COUPON_HAS_EXPIRED("0028", "优惠券已过期!"),
     MANUAL_COUPON_CANNOT_BE_ISSUED("0029", "手动领取方式优惠券不能发放!"),
     ISSUE_TIME_MUST_GREATER_CURRENT_TIME("0030", "发放时间要大于当前时间!"),
-    COUPON_END_DATE_MUST_GREATER_CURRENT_DATE("0031", "优惠券结束日要大于当前日期!"),
+    COUPON_END_DATE_MUST_GREATER_CURRENT_DATE("0031", "发放日期要小于优惠券有效截止日期!"),
     COUPON_ISSUE_QUANTITY_CANNOT_LESS_ZERO("0032", "优惠券发行数量不能小于等于0!"),
     INVISIBLE_COUPON_ISSUE_TASK_CANNOT_BE_ISSUED("0033", "优惠券发放任务已经下架，不能实施优惠券发放"),
     COUPON_NOT_ISSUE_STATUS_CAN_DELETE("0034", "未发放状态的优惠券发放才可以删除!"),
